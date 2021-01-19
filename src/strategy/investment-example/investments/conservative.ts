4e46957e6120656678e0dcea1f9da8e353932b76
Diego Lopes <diego-chez@hotmail.com>
2021-01-18 21:39:57 -0300

    add conservative investment class

---
+++ src/strategy/investment-example/investments/conservative.ts
@@ -0,0 +1,9 @@
+import { BankAccount } from "../bank-account";
+import { Investment } from "./protocols/investment";
+
+export class ConvervativeInvestment implements Investment {
+    public getProfit(bankAccount: BankAccount): number {
+        const balance = bankAccount.checkBalance()
+        return  balance.value * 0.8
+    }
+}
