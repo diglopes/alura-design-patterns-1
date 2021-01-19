00da66161bb96744d327e147be3fe393e802e828
Diego Lopes <diego-chez@hotmail.com>
2021-01-18 21:56:42 -0300

    adjust profit rate

--- src/strategy/investment-example/investments/conservative.ts
+++ src/strategy/investment-example/investments/conservative.ts
@@ -4,6 +4,6 @@ import { Investment } from "./protocols/investment";
 export class ConvervativeInvestment implements Investment {
     public getProfit(bankAccount: BankAccount): number {
         const balance = bankAccount.checkBalance()
-        return  balance.value * 0.8
+        return  balance.value * 0.08
     }
 }
