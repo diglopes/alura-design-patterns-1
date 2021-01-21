57998872f364fe75f01b4f6a77b14ebb368d4612
Diego Lopes <diego-chez@hotmail.com>
2021-01-21 12:11:18 -0300

    change base response to a normal class and return stringified data

--- src/chain-of-responsibility/bank-example/responses/base-response.ts
+++ src/chain-of-responsibility/bank-example/responses/base-response.ts
@@ -2,11 +2,11 @@ import { Request } from "../request";
 import { Response } from "./protocols/response";
 import { Account } from "../account"
 
-export abstract class BaseResponse implements Response {
+export class BaseResponse implements Response {
   protected next: Response
 
   public handle(req: Request, account: Account): string {
-    return "";
+    return JSON.stringify(account.getData())
   }
 
   public setNext(response: Response): void {
--- src/chain-of-responsibility/bank-example/responses/response-container.ts
+++ src/chain-of-responsibility/bank-example/responses/response-container.ts
@@ -1,5 +1,6 @@
 import { Account } from "../account";
 import { Request } from "../request";
+import { BaseResponse } from "./base-response";
 import { CSVResponse } from "./csv-response";
 import { XMLResponse } from "./xml-response";
 
@@ -7,7 +8,11 @@ export class ResponseContainer {
   public send(req: Request, account: Account): string {
     const xmlResponse = new XMLResponse();
     const csvResponse = new CSVResponse()
+    const baseResponse = new BaseResponse()
+
     xmlResponse.setNext(csvResponse)
+    csvResponse.setNext(baseResponse)
+    
     return xmlResponse.handle(req, account)
   }
 }
