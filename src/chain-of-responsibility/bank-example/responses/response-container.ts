d19878854f70c9a7eac603025dead55131dfca9f
Diego Lopes <diego-chez@hotmail.com>
2021-01-21 12:10:30 -0300

    add csv response to container

--- src/chain-of-responsibility/bank-example/responses/response-container.ts
+++ src/chain-of-responsibility/bank-example/responses/response-container.ts
@@ -1,10 +1,13 @@
 import { Account } from "../account";
 import { Request } from "../request";
+import { CSVResponse } from "./csv-response";
 import { XMLResponse } from "./xml-response";
 
 export class ResponseContainer {
   public send(req: Request, account: Account): string {
     const xmlResponse = new XMLResponse();
+    const csvResponse = new CSVResponse()
+    xmlResponse.setNext(csvResponse)
     return xmlResponse.handle(req, account)
   }
 }
